817af30507b6317558be997b3384888b874fc72d
moskalyk <morgan.moskalyk@protonmail.ch>
2023-10-01 14:26:58 -0400

    removed compiled files

--- server/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-// @ts-ignore
-var corestore_1 = require("corestore");
-var corestore = new corestore_1.default('./db');
-var db = corestore.get({ name: "numbers", valueEncoding: 'json' });
-exports.db = db;
